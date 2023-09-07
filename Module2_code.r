7e798764cb0ce46a01b81d8a293dcc88b4ed1c37
Amira-AlSamawi <128369467+Amira-AlSamawi@users.noreply.github.com>
2023-09-07 16:25:14 +0300

    Update and rename FullCode.r to Module2_code.r

---
+++ Module2_code.r
@@ -0,0 +1,17 @@
+library(terra)
+library(sf)
+library(randomForest)
+library(lpSolve)
+library(DescTools)
+library(irr)
+
+Load the cropped sentinel data 
+sentinel1 = rast("D:/GeoTraining Day 3/sentinel2_NDVI.tif")
+sentinel2_nov= rast("D:/GeoTraining Day 3/sentinel2_20221127_ndvi.tif")
+sentinel= c(sentinel1,sentinel2_nov)
+
+#Load the polygons: can also be a .shp file
+#Ask if layer over layer
+polygons2= st_read("D:/GeoTraining Day 3/example2.gpkg")
+sentinel
+polygons2
