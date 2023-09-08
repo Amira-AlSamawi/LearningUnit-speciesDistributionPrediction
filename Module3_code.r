b0351453028c39ae5d6e9367b3037b3f600a8016
Amira-AlSamawi <128369467+Amira-AlSamawi@users.noreply.github.com>
2023-09-08 15:08:40 +0300

    Update Module3_code.r

--- Module3_code.r
+++ Module3_code.r
@@ -1,68 +1,8 @@
 
-library(randomForest)
 library(lpSolve)
 library(DescTools)
 library(irr)
 
-
-
-#Random forest algorithm
-#y= classes and x= predictors from satellite 
-rfmodel= randomForest(x= df[, c("B02_nov", "B03_nov","B04_nov", "B08_nov", "NDVI_nov", "B02", "B03","B04", "B08", "NDVI")],
-                      y= as.factor(df[, c("class")]),ntree=100)
-
-#Apply the model to the sentinel image
-lcc= predict
-lcc= predict(sentinel, rfmodel)
-plot(lcc
-
-plot(lcc, col= c("brown","green2", "black","blue","brown2", "white","green" ))
-
-writeRaster(lcc, "D:/GeoTraining Day 3/LandCoverLayer.tif")
-
-#Transform Geodetic CRS - Sentinel
-polygons= st_transform(polygons, crs = st_crs(sentinel))
-sentinel= c(sentinel1,sentinel2_nov)
-
-#Extract
-df= extract(sentinel, polygons)
-View(df)
-polygons$ID= seq(1, nrow(polygons))
-
-df= merge(df, polygons)
-View(df)
-
-#Split all data into training and test
-test_sample= sample(nrow(df), nrow(df)/4)
-test_set= df[test_sample,]
-training_set= df[test_sample,]
-View(training_set)
-
-#Random Forest Algorithm
-#y= classes and x= predictors from satellite 
-rfmodel= randomForest(x= training_set[, c("B02_nov", "B03_nov","B04_nov", "B08_nov", "NDVI_nov")],
-                      y= as.factor(training_set[, c("class")]),ntree=100)
-
-#Apply the model to the sentinel image
-lcc= predict(sentinel, rfmodel)
-
-#Predict the classes of the independent test set
-test_prediction= predict(rfmodel, test_set)
-print(test_prediction)
-
-#CohenKappa
-tabletest= table(test_set$class, test_prediction)
-CohenKappa(tabletest)
-
-#FleissKappa
-kappam.fleiss(tabletest)
-
-#Confusion matrix
-cfm= table(test_set$class, test_prediction)
-View(cfm)
-
-#Accuracy: sum of diagonals divided by all
-sum(diag(cfm))/sum(cfm)
 
 #Load csv
 fogo_species = read.csv("D:/Geotraining Day 4/fogo_species.csv")
